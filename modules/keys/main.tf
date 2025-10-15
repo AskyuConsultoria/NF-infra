67f5b6f7d232e0fb9c0752c0788a26492d4a7058
Kaykybelarmino <kayky.pedroso@sptech.school>
2025-10-15 12:32:22 -0300

    Fix: ajuste nas chaves

--- modules/keys/main.tf
+++ modules/keys/main.tf
@@ -1,14 +1,18 @@
+variable "pbkey-ges-pub" {}
+variable "pvkey-ges-pub" {}
+variable "pbkey-jupyter-pub" {}
+
 resource "aws_key_pair" "main-public" {
   key_name   = "pbkey-ges"
-  public_key = file("~/.ssh/pbkey-ges.pub")
+  public_key = var.pbkey-ges-pub
 }
 
 resource "aws_key_pair" "main-backend" {
   key_name   = "pvkey-ges"
-  public_key = file("~/.ssh/pvkey-ges.pub")
+  public_key = var.pvkey-ges-pub
 }
 
 resource "aws_key_pair" "jupyter" {
   key_name   = "pbkey-jupyter"
-  public_key = file("~/.ssh/pbkey-jupyter.pub")
+  public_key = var.pbkey-jupyter-pub
 }
